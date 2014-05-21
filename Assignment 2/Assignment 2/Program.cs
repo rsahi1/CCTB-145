2e4237434df5655c8f8d372fd3d2e0a68e84a0fe
rsahi1 <raman.sahi@hotmail.com>
2014-05-21 15:59:41 -0600

    Assignment 2
    Need to finish it

---
+++ Assignment 2/Assignment 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        bool attendingSchool = false;
+        bool workingFullTime = true;
+       string name = "Raman";
+       string city ="Edmonton";
+        int yearsCollege =2;
+        int yearsUniversity = 4;
+        int totalPostSecondaryYears = 6;
+        bool attendingSchoolandWorking = false;
+        bool moreThan4YearsPostSecondary = true;
+        bool hardWorkingorSmart = true;
+            // TODO: continue...
+        //Console.WriteLine ("
+        }
+    }
+}

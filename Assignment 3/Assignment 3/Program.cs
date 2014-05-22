f1dbd16d2e6a83e1302f3ed053799a371c078a4a
rsahi1 <raman.sahi@hotmail.com>
2014-05-22 13:47:34 -0600

    Hello World
    Made changes to Hello World

--- Assignment 3/Assignment 3/Program.cs
+++ Assignment 3/Assignment 3/Program.cs
@@ -10,7 +10,33 @@ namespace Assignment_3
     {
         static void Main(string[] args)
         {
+            int Number;
+            {
+            Console.WriteLine("Enter a number between 1 and 100");
+                Number = int.Parse(Console.ReadLine());
+            }
+            if (Number >1 || Number < 100)
+            {
+            Console.WriteLine ("Number is in between the allowed range of numbers");
+            }
 
-        }
+            else if (Number < 1 || Number > 100)
+            {
+                Console.WriteLine("Number is not between the allowed range of numbers");
+            }
+        } // End of step 2 method
+           
+            private void forStatementSample()
+            {
+                for( int currentNumber = 1; currentNumber <= 100; currentNumber++)
+                {
+                    Console.WriteLine (currentNumber);
+                }
+
+    }
+            static public bool IsEvenNumber(int intNumber)
+            {
+                return intNumber % 2 == 0;
+            }
     }
 }
